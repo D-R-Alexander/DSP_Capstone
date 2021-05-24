ad995b0c636e346f71a5424dd4537c641f99bd86
Daniel Alexander <72933701+D-R-Alexander@users.noreply.github.com>
2021-05-23 20:47:25 -0400

    Code complete and functioning with acceptable RMSE

--- MovieLens.R
+++ MovieLens.R
@@ -49,46 +49,152 @@ removed <- anti_join(temp, validation)
 edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
+#######################################
+##### END OF COURSE-PROVIDED CODE #####
+#######################################
 
-#Q2a: How many zeros were given as ratings in the edx dataset?
-sum(edx$rating == 0)
+##### EXPLORATION #####
+# What is the average rating
+mu <- mean(edx$rating)
 
-#Q2a: How many threes were given as ratings in the edx dataset?
-sum(edx$rating == 3)
-
-#Q3 How many different movies are in the edx dataset?
-length(unique(edx$movieId))
-
-#Q4 How many different users are in the edx dataset?
-length(unique(edx$userId))
-
-#Q5 How many movie ratings are in each of the following genres in the edx dataset?
+# Learned in Pt. 1 that some genres received many more ratings than others
+# Explore distribution - quantity of ratings per genre
 edx %>% separate_rows(genres, sep = "\\|") %>%
-  group_by(genres) %>%
-  summarize(count = n())
+  count(genres) %>%
+  ggplot(aes(n)) +
+  geom_histogram(bins = 30, color = "black") +
+  scale_x_log10() +
+  ggtitle("Number of Ratings per Genre")
+# Result: Two genres received significantly fewer ratings than the others.
+# The majority received 100K or more ratings with 7 of 20 over 1M.
 
-#Q6 Which movie has the greatest number of ratings?
-numRatings <- edx %>% group_by(movieId) %>% 
-  summarize(numRatings = n(), movieTitle = first(title)) %>%
-  arrange(desc(numRatings)) %>%
-  top_n(10, numRatings)
+# Explore if positive or negative bias by genre
+genre_bias_avg <- edx %>% 
+  separate_rows(genres, sep = "\\|") %>%
+  group_by(genres) %>% 
+  summarize(g_bias = mean(rating - mu))
 
-#Q7 What are the five most given ratings in order from most to least?
-numRatings <- edx %>% group_by(rating) %>% 
-  summarize(number = n())
+genre_bias_avg %>%
+  ggplot(aes(g_bias)) +
+  geom_histogram(bins = 30, color = "black") + 
+  ggtitle("Bias of Ratings by Genre")
+# Result: More genres ~14 of the 20 received ratings higher than the average
 
-numRatings %>% top_n(5) %>% arrange(desc(number))
-
-#Q8 True or False: In general, half star ratings are less common than whole 
-# star ratings (e.g., there are fewer ratings of 3.5 than there are ratings 
-# of 3 or 4, etc.)
-numRatings %>%
-  mutate(halfStar = rating %% 1 == 0.5) %>%
-  group_by(halfStar) %>%
-  summarize(number = sum(number))
-
-edx %>%
+# Learned in Pt. 1 that some some ratings were more common than others
+# Explore distribution of ratings
+edx %>% 
   group_by(rating) %>%
-  summarize(count = n()) %>%
-  ggplot(aes(x = rating, y = count)) +
-  geom_line()
+  ggplot(aes(rating)) +
+  geom_histogram(bins = 20, color = "black") +
+  scale_x_log10() +
+  ggtitle("Distribution of Ratings")
+# Result: Majority of ratings are over 3.0
+
+# Explore distribution - quantity of ratings per movie
+edx %>% 
+  count(movieId) %>%
+  ggplot(aes(n)) +
+  geom_histogram(bins = 30, color = "black") +
+  scale_x_log10() +
+  ggtitle("Number of Ratings per Movie")
+# Result: Majority of movies received between 50-500 ratings
+
+# Explore distribution - quantity of ratings per user
+edx %>% count(userId) %>% 
+  ggplot(aes(n)) + 
+  geom_histogram(bins = 30, color = "black") + 
+  scale_x_log10() + 
+  ggtitle("Number of Ratings per User")
+# Result: Most raters submitted 100 or fewer ratings.
+
+# Explore rater tendencies
+rater_bias_avg <- edx %>% 
+  group_by(userId) %>%
+  summarize(r_bias = mean(rating - mu))
+rater_bias_avg %>%
+  ggplot(aes(r_bias)) +
+  geom_histogram(bins = 30, color = "black") +
+  ggtitle("Rater Tendencies")
+# Result: Skewed toward more positive ratings
+
+# Explore if certain movies are rated differently than others
+movie_bias_avg <- edx %>% 
+  group_by(movieId) %>% 
+  summarize(m_bias = mean(rating - mu))
+
+movie_bias_avg %>%
+  ggplot(aes(m_bias)) +
+  geom_histogram(bins = 30, color = "black") +
+  ggtitle("Bias of Ratings by Movie")
+# Result: More movies received ratings lower than the average
+
+# Explore how rater tendencies impact movie ratings
+r_m_bias_avg <- edx %>% 
+  left_join(movie_bias_avg, by = "movieId") %>%
+  group_by(userId) %>%
+  summarize(r_m_bias = mean(rating - mu - m_bias))
+r_m_bias_avg %>%
+  ggplot(aes(r_m_bias)) +
+  geom_histogram(bins = 30, color = "black") +
+  ggtitle("Rater Tendency Impact on Movie Ratings")
+# Result shows a standard distribution
+
+##### FUNCTIONS #####
+
+#RMSE
+rmse <- function(actualRatings, predictRatings){
+  sqrt(mean((actualRatings - predictRatings)^2, na.rm = TRUE))
+}
+
+##### BEGIN #####
+# Need something to hold and display results
+results <- data.frame()
+
+# Develop and Test Models on edx Data
+
+# Model 1 - Naive using only mean rating
+m1_rmse <- RMSE(edx$rating,mu)
+results <- bind_rows(results, data_frame(model="M1 Naive", finalRMSE = m1_rmse))
+
+# Model 2 - Effect of Genre 
+pred_rating_genre <- edx %>%
+  separate_rows(genres, sep = "\\|") %>%
+  left_join(genre_bias_avg, by = "genres") %>%
+  mutate(prediction = mu + g_bias)
+m2_rmse <- RMSE(edx$rating,pred_rating_genre$prediction)
+results <- bind_rows(results, data_frame(model = "M2 Genre", finalRMSE = m2_rmse))
+
+# Model 3 - Effect of Movie
+pred_rating_movie <- edx %>%
+  left_join(movie_bias_avg, by = "movieId") %>%
+  mutate(prediction = mu + m_bias)
+m3_rmse <- RMSE(edx$rating,pred_rating_movie$prediction)
+results <- bind_rows(results, data_frame(model = "M3 Movie", finalRMSE = m3_rmse))
+
+# Model 4 - Effect of Rater
+pred_rating_rater <- edx %>%
+  left_join(rater_bias_avg, by = "userId") %>%
+  mutate(prediction = mu + r_bias)
+m4_rmse <- RMSE(edx$rating,pred_rating_rater$prediction)
+results <- bind_rows(results, data_frame(model = "M4 Rater", finalRMSE = m4_rmse))
+
+
+# Model 5 - Effect of Rater and Movie
+pred_rating_r_m <- edx %>%
+  left_join(movie_bias_avg, by = "movieId") %>%
+  left_join(r_m_bias_avg, by = "userId") %>%
+  mutate(prediction = mu + m_bias + r_m_bias)
+m5_rmse <- RMSE(edx$rating,pred_rating_r_m$prediction)
+results <- bind_rows(results, data_frame(model = "M5 Rater and Movie", finalRMSE = m5_rmse))
+
+results
+
+# Test on Validation Set 
+
+pred_rating_final <- validation %>%
+  left_join(movie_bias_avg, by = "movieId") %>%
+  left_join(r_m_bias_avg, by = "userId") %>%
+  mutate(prediction = mu + m_bias + r_m_bias)
+finalResult <- RMSE(validation$rating,pred_rating_final$prediction)
+
+finalResult
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,10 +1,36 @@
-You will use the following code to generate your datasets. Develop your algorithm using the edx set. 
-For a final test of your final algorithm, predict movie ratings in the validation set (the final 
-hold-out test set) as if they were unknown. RMSE will be used to evaluate how close your predictions 
-are to the true values in the validation set (the final hold-out test set).
+Project Requirements:
 
-Important: The validation data (the final hold-out test set) should NOT be used for training, developing, 
-or selecting your algorithm and it should ONLY be used for evaluating the RMSE of your final algorithm. 
-The final hold-out test set should only be used at the end of your project with your final model. It may 
-not be used to test the RMSE of multiple models during model development. You should split the edx data 
-into separate training and test sets to design and test your algorithm.
+1. Report in Rmd format
+2. Report in PDF format (knit from your Rmd file)
+3. Script in R format that generates predicted movie ratings and RMSE score 
+    (should contain all code and comments for your project)
+
+REPORT:
+Documents the analysis and presents the findings, along with supporting statistics and figures. The report must be written in English and uploaded. The report should be written assuming that the reader is not familiar with the project or the data. The report must include the RMSE generated. The report must include at least the following sections:
+
+- Introduction/overview/executive summary section that describes the dataset and summarizes the goal of the project and key steps that were performed
+
+- Methods/analysis section that explains the process and techniques used, including data cleaning, data exploration and visualization, insights gained, and your modeling approach
+
+- Results section that presents the modeling results and discusses the model performance
+
+- Conclusion section that gives a brief summary of the report, its limitations and future work
+
+CODE:
+The code in the R script should should be well-commented and easy to follow. The code provided in the R script should contain all of the code and comments 
+for your project. You are not required to run the code provided (although you may if you wish), but you should visually inspect it. You will use the following code to generate your datasets. Develop your algorithm using the edx set. 
+
+RMSE:
+Provide the appropriate score given the reported RMSE. Please be sure not to use the validation set (the final hold-out test set) for training or regularization - you should create an additional partition of training and test sets from the provided edx dataset to experiment with multiple parameters or use cross-validation
+
+- 20%: RMSE >= 0.90000 AND/OR the reported RMSE is the result of overtraining (validation set - the final hold-out test set - ratings used for anything except reporting the final RMSE value) AND/OR the reported RMSE is the result of simply copying and running code provided in previous courses in the series
+
+- 40%: 0.86550 <= RMSE <= 0.89999
+- 60%: 0.86500 <= RMSE <= 0.86549
+- 80%: 0.86490 <= RMSE <= 0.86499
+- 100%: RMSE < 0.86490
+
+##########
+For a final test of your final algorithm, predict movie ratings in the validation set (the final hold-out test set) as if they were unknown. RMSE will be used to evaluate how close your predictions are to the true values in the validation set (the final hold-out test set).
+
+Important: The validation data (the final hold-out test set) should NOT be used for training, developing, or selecting your algorithm and it should ONLY be used for evaluating the RMSE of your final algorithm. The final hold-out test set should only be used at the end of your project with your final model. It may not be used to test the RMSE of multiple models during model development. You should split the edx data into separate training and test sets to design and test your algorithm.
